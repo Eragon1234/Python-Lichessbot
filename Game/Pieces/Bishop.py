38ea8dd994636c50e491ed99ede6d5327f81091a
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-11 22:38:41 +0100

    adding a function to the bishop object that returns all potential new positions of the bishop

--- Game/Pieces/Bishop.py
+++ Game/Pieces/Bishop.py
@@ -3,3 +3,24 @@ class Bishop:
 
     def __init__(self, isWhite):
         self.isWhite = isWhite
+
+    def generatePossiblePositions(self, currentPosition):
+        positions = []
+
+        diagonal1 = [currentPosition[0], currentPosition[1]]
+        while (diagonal1[0] != 1 and diagonal1[1] != 1):
+            diagonal1 = [diagonal1[0] - 1, diagonal1[1] - 1]
+
+        diagonal2 = [currentPosition[0], currentPosition[1]]
+        while (diagonal2[0] != 1 and diagonal2[1] != 1):
+            diagonal2 = [diagonal2[0] + 1, diagonal2[1] - 1]
+        
+        position = [diagonal1[0], diagonal1[1]]
+        while (position[0] != 9 and position[1] != 9):
+            positions.push(position)
+            position = [position[0] + 1, position[1] + 1]
+
+        position = [diagonal2[0], diagonal2[1]]
+        while (position[0] != 9 and position[1] != 9):
+            positions.push(position)
+            position = [position[0] - 1, position[1] + 1]
