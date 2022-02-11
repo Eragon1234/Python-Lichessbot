d7b58f52333daeeb7eac1664b14a4f29f64aef0b
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-11 22:37:56 +0100

    adding a function to the pawn object that returns all potential new positions of the pawn

--- Game/Pieces/Pawn.py
+++ Game/Pieces/Pawn.py
@@ -3,3 +3,72 @@ class Pawn:
 
     def __init__(self, isWhite):
         self.isWhite = isWhite
+
+    def generatePossiblePositions(self, currentPosition):
+        positions = []
+        if self.isWhite:
+            position = [
+                currentPosition[0],
+                currentPosition[1] - 1
+            ]
+            positions.append(position)
+
+            position = [
+                currentPosition[0] - 1,
+                currentPosition[1] - 1,
+                [
+                    'isEnemyPiece',
+                    'isEnPassantField'
+                ]
+            ]
+            positions.append(position)
+
+            position = [
+                currentPosition[0] + 1,
+                currentPosition[1] - 1,
+                [
+                    'isEnemyPiece',
+                    'isEnPassantField'
+                ]
+            ]
+            positions.append(position)
+
+            if currentPosition[1] == 6:
+                position = [
+                    currentPosition[0],
+                    currentPosition[1] - 2
+                ]
+                positions.append(position)
+        else:
+            position = [
+                currentPosition[0],
+                currentPosition[1] + 1
+            ]
+            positions.append(position)
+
+            position = [
+                currentPosition[0] - 1,
+                currentPosition[1] + 1,
+                [
+                    'isEnemyPiece',
+                    'isEnPassantField'
+                ]
+            ]
+            positions.append(position)
+
+            position = [
+                currentPosition[0] + 1,
+                currentPosition[1] + 1,
+                [
+                    'isEnemyPiece',
+                    'isEnPassantField'
+                ]
+            ]
+            positions.append(position)
+
+            if currentPosition[1] == 1:
+                position = [
+                    currentPosition[0],
+                    currentPosition[1] + 2
+                ]
+                positions.append(position)
