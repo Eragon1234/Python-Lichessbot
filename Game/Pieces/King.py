b5f046b9f21d0a4da147ca396edb1f2ad4113fe2
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-12 12:52:40 +0100

    adding a function to the king object that returns all potential new positions of the king

--- Game/Pieces/King.py
+++ Game/Pieces/King.py
@@ -3,3 +3,15 @@ class King:
 
     def __init__(self, isWhite):
         self.isWhite = isWhite
+
+    def generatePossiblePositions(self, currentPosition):
+        positions = []
+        for x in range(currentPosition[0] - 1, currentPosition[0] + 2):
+            for y in range(currentPosition[1] - 1, currentPosition[1] + 2):
+                position = [
+                    x,
+                    y
+                ]
+                if (position[0] >= 0 and position[1] >= 0 and position[0] <= 7 and position[1] <= 7 and position != currentPosition):
+                    positions.append(position)
+        return positions
