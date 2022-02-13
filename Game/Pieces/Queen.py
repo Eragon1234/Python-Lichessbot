47235978e603ae846c63b83c75aa385143a99ac2
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-13 12:05:12 +0100

    adding a function to the queen object that returns all potential new positions of the queen

--- Game/Pieces/Queen.py
+++ Game/Pieces/Queen.py
@@ -1,5 +1,16 @@
+from Bishop import Bishop
+from Rook import Rook
+
 class Queen:
     value = 9
 
     def __init__(self, isWhite):
         self.isWhite = isWhite
+
+    def generatePossiblePositions(self, currentPosition):
+        bishop = Bishop(self.isWhite)
+        rook = Rook(self.isWhite)
+        rookPositions = rook.generatePossiblePositions(currentPosition)
+        bishopPositions = bishop.generatePossiblePositions(currentPosition)
+        positions = rookPositions + bishopPositions
+        return positions
