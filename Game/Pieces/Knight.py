5bcbf65578b902bca8cd804f93143827146b00b4
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-13 12:25:14 +0100

    adding a function to the knight object that returns all potential new positions of the knight

--- Game/Pieces/Knight.py
+++ Game/Pieces/Knight.py
@@ -3,3 +3,26 @@ class Knight:
 
     def __init__(self, isWhite):
         self.isWhite = isWhite
+    
+    def generatePossiblePositions(self, currentPosition):
+        positions = []
+        position = (currentPosition[0] - 1, currentPosition[1] - 2)
+        positions.append(position)
+        position = (currentPosition[0] + 1, currentPosition[1] - 2)
+        positions.append(position)
+        position = (currentPosition[0] + 2, currentPosition[1] - 1)
+        positions.append(position)
+        position = (currentPosition[0] + 2, currentPosition[1] + 1)
+        positions.append(position)
+        position = (currentPosition[0] + 1, currentPosition[1] + 2)
+        positions.append(position)
+        position = (currentPosition[0] - 1, currentPosition[1] + 2)
+        positions.append(position)
+        position = (currentPosition[0] - 2, currentPosition[1] + 1)
+        positions.append(position)
+        position = (currentPosition[0] - 2, currentPosition[1] - 1)
+        positions.append(position)
+        positions = list(filter(lambda position: position[0] >= 0 and position[0] <= 7 and position[1] >= 0 and position[1] <= 7, positions))
+        return positions
+knight = Knight(True)
+print(len(knight.generatePossiblePositions((1, 1))))
