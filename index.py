190c97864ba1d12e0cf61cb23945ecaf599e58c4
Alexander Gerstberger <a.gerstberger@web.de>
2022-02-17 19:28:01 +0100

    adding comments and documentation to the index.py and the GameController object

--- index.py
+++ index.py
@@ -1,13 +1,22 @@
+# importing the GameController for handling the connetion to the Lichess API
 from GameController import GameController
+
+# importing our Engine
 from Engine import Engine
 
+# initializing our GameController
 game = GameController()
 
+# justa accepting challenges and streaming games without further processing
 game.on('challenge', game.acceptChallenge)
 game.on('gameStart', game.streamGame)
 
+# initializing our Engine
 engine = Engine()
+
+# adding the methods of the engine to handle the engines turn and the incoming moves of the opponent
 game.on('myMove', engine.move)
 game.on('opponentsMove', engine.opponentsMove)
 
+# starting to watch for events
 game.watch()
