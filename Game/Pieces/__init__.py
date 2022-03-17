4c2c9eaf5e7fc8fab694c6e3f9bb00666a61da97
Alexander Gerstberger <a.gerstberger@web.de>
2022-03-17 21:19:36 +0100

    refactoring the pieces into a package instead of a module

---
+++ Game/Pieces/__init__.py
@@ -0,0 +1,7 @@
+from .EmtpyField import EmptyField
+from .Pawn import Pawn
+from .Bishop import Bishop
+from .Knight import Knight
+from .Rook import Rook
+from .Queen import Queen
+from .King import King
