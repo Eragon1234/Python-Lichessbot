ec461c2dbd2bc1d354edfeec60132432c96aea1b
Alexander Gerstberger <a.gerstberger@web.de>
2023-05-05 12:52:16 +0200

    moving index.py contents into a main function

--- index.py
+++ index.py
@@ -5,27 +5,33 @@ from engine import Engine
 from formatter import MyFormatter
 from game_controller import GameController
 
-logging.basicConfig(level=logging.INFO)
-streamHandler = logging.StreamHandler()
-streamHandler.setFormatter(MyFormatter())
-logging.getLogger().handlers.clear()
-logging.getLogger().addHandler(streamHandler)
 
-lichess_token = os.getenv("LICHESS_TOKEN")
+def main():
+    logging.basicConfig(level=logging.INFO)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(MyFormatter())
+    logging.getLogger().handlers.clear()
+    logging.getLogger().addHandler(stream_handler)
 
-# initializing our GameController
-game = GameController(lichess_token)
+    lichess_token = os.getenv("LICHESS_TOKEN")
 
-# just accepting challenges and streaming games without further processing
-game.on('challenge', game.accept_challenge)
-game.on('game_start', game.stream_game)
+    # initializing our GameController
+    game = GameController(lichess_token)
 
-# initializing our Engine
-engine = Engine()
+    # just accepting challenges and streaming games without further processing
+    game.on('challenge', game.accept_challenge)
+    game.on('game_start', game.stream_game)
 
-# adding the methods of the engine to handle the engines turn and the incoming moves of the opponent
-game.on('my_move', engine.move)
-game.on('opponents_move', engine.opponents_move)
+    # initializing our Engine
+    engine = Engine()
 
-# starting to watch for events
-game.watch()
+    # adding the methods of the engine to handle the engines turn and the incoming moves of the opponent
+    game.on('my_move', engine.move)
+    game.on('opponents_move', engine.opponents_move)
+
+    # starting to watch for events
+    game.watch()
+
+
+if __name__ == '__main__':
+    main()
