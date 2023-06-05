4fe5ae3780ca226b0e5798e76612f880508a7b99
Alexander Gerstberger <a.gerstberger@web.de>
2023-06-05 21:53:35 +0200

    adding docstrings to event_emitter.py

--- event_emitter.py
+++ event_emitter.py
@@ -7,11 +7,13 @@ class EventEmitter:
         self.handlers = {}
 
     def on(self, event: str, handler: Callable[..., None]):
+        """Registers a handler for an event."""
         if event not in self.handlers:
             self.handlers[event] = []
 
         self.handlers[event].append(handler)
 
     def emit(self, event: str, *args):
+        """Emits an event."""
         for handler in self.handlers[event]:
             handler(*args)
