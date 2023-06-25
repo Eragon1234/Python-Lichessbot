ed0186d9445e51698c911a6d337d6b0426567203
Alexander <a.gerstberger@web.de>
2023-06-25 15:18:33 +0200

    using unittests subTests for our table driven tests in test_king.py

--- game/pieces/test_king.py
+++ game/pieces/test_king.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from unittest import TestCase
 
 from game import King
@@ -7,9 +7,8 @@ from game import King
 class TestKing(TestCase):
     def test_generate_possible_positions(self):
         class Case:
-            def __init__(self, position: (int, int), message: str, expected: List[Tuple[int, int]]):
+            def __init__(self, position: Tuple[int, int], expected: List[Tuple[int, int]]):
                 self.position = position
-                self.message = message
                 self.expected = expected
                 self.board: List[List[str | bool]] = [["EmptyField" for _ in range(8)] for _ in range(8)]
 
@@ -17,30 +16,31 @@ class TestKing(TestCase):
                 self.board[y][x] = color
                 return self
 
-        test_cases: List[Case] = [
-            Case((0, 0), "King shouldn't move out of the board", [
+        test_cases: Dict[str, Case] = {
+            "king in the corner": Case((0, 0), [
                 (1, 0),
                 (0, 1), (1, 1)
             ]),
-            Case((4, 4), "King should move to all adjacent positions", [
+            "all empty fields": Case((4, 4), [
                 (3, 3), (4, 3), (5, 3),
                 (3, 4), (5, 4),
                 (3, 5), (4, 5), (5, 5)
             ]),
-            Case((4, 4), "King shouldn't move to positions occupied by own pieces", [
+            "field occupied by own piece": Case((4, 4), [
                 (3, 3), (4, 3), (5, 3),
                 (5, 4),
                 (3, 5), (4, 5), (5, 5)
             ]).with_piece(True, 3, 4),
-            Case((4, 4), "King should move to positions occupied by enemy pieces", [
+            "field occupied by enemy piece": Case((4, 4), [
                 (3, 3), (4, 3), (5, 3),
                 (3, 4), (5, 4),
                 (3, 5), (4, 5), (5, 5)
-            ]).with_piece(False, 3, 4),
-        ]
+            ]).with_piece(False, 3, 4)
+        }
 
         king = King(True)
-        for test_case in test_cases:
-            actual = king.generate_possible_positions(test_case.position, test_case.board)
+        for name, test_case in test_cases.items():
+            with self.subTest(name):
+                actual = king.generate_possible_positions(test_case.position, test_case.board)
 
-            self.assertCountEqual(test_case.expected, actual, test_case.message)
+                self.assertCountEqual(test_case.expected, actual)
