f77ad543f387b050863f137d7a8182a7311a06b6
Alexander <a.gerstberger@web.de>
2023-06-26 11:58:56 +0200

    adding table-based tests for pawn

---
+++ game/pieces/test_pawn.py
@@ -0,0 +1,46 @@
+from typing import Dict, Tuple, List
+from unittest import TestCase
+
+from game import Pawn
+
+
+class TestPawn(TestCase):
+    def test_generate_possible_positions(self):
+        class Case:
+            def __init__(self, position: Tuple[int, int], expected: List[Tuple[int, int]]):
+                self.position = position
+                self.expected = expected
+                self.board: List[List[str | bool]] = [["EmptyField" for _ in range(8)] for _ in range(8)]
+
+            def with_piece(self, color: bool, x: int, y: int):
+                self.board[y][x] = color
+                return self
+
+        test_cases: Dict[str, Case] = {
+            "empty board": Case((4, 4), [
+                (4, 5)
+            ]),
+            "field in front occupied by own piece": Case((4, 4), []).with_piece(True, 4, 5),
+            "field in front occupied by enemy piece": Case((4, 4), []).with_piece(False, 4, 5),
+            "field left front occupied by enemy piece": Case((4, 4), [
+                (3, 5),
+                (4, 5)
+            ]).with_piece(False, 3, 5),
+            "field right front occupied by enemy piece": Case((4, 4), [
+                (5, 5),
+                (4, 5)
+            ]).with_piece(False, 5, 5),
+            "field right front occupied by own piece": Case((4, 4), [
+                (4, 5)
+            ]).with_piece(True, 5, 5),
+            "field left front occupied by own piece": Case((4, 4), [
+                (4, 5)
+            ]).with_piece(True, 3, 5),
+        }
+
+        pawn = Pawn(True)
+        for name, test_case in test_cases.items():
+            with self.subTest(name):
+                actual = pawn.generate_possible_positions(test_case.position, test_case.board)
+
+                self.assertCountEqual(test_case.expected, actual)
