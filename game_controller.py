7d235bfe2a901d76196efaf6e0491fa1475b5e62
Alexander Gerstberger <a.gerstberger@web.de>
2023-07-08 22:35:30 +0200

    removing comments that describe what the code is doing from game_controller.py

--- game_controller.py
+++ game_controller.py
@@ -15,52 +15,38 @@ class GameController:
         self.color = None
 
     def watch(self) -> typing.NoReturn:
-        """subscribes to the lichess api to watch for events as a challenge, gameStart etc. and emits the belonging
-        events"""
+        """subscribes to the lichess api and emits the events"""
         for event in self.client.stream_events():
-            # checking if the event type is a challenge
             if event['type'] == 'challenge':
-                # getting the challenge_id and the challenger
                 challenge_id = event['challenge']['id']
 
-                # throwing the challenge event with the challenge_id, the challenger and the function to accept
-                # the challenge as params
                 self.emitter.emit('challenge', challenge_id, self.client.accept_challenge)
-
-            # checking if the event type is gameStart
             elif event['type'] == 'gameStart':
-
-                # getting the game_id, the color the engine is playing and data about the opponent
                 game_id = event['game']['gameId']
                 self.color = PlayerColor(event['game']['color'])
                 opponent = event['game']['opponent']
 
-                # throwing the gameStart event with the game_id, the opponent and the function to start streaming
-                # the game
                 self.emitter.emit('game_start', game_id, opponent, self.stream_game)
 
     def on(self, event: str, fn: typing.Callable[[str, ...], None]) -> None:
         """ adds functions to be called on the mentioned incoming events
 
-        Args:
+        Args
             event (str): the event to be handled
             fn (callable): the function to be assigned to the passed event
         """
         self.emitter.on(event, fn)
 
     def stream_game(self, game_id: str, *args) -> None:
-        """ Subscribing to the stream of events for the game with the passed gameId
+        """ Subscribes to the events for the game with and emits the events
 
         Args:
             game_id (str): the gameId of the game to be subscribed to
         """
         for event in self.client.stream_game(game_id):
-            # checking if there is an object with the key of 'state' in the response
             if 'state' in event.keys():
-                # getting the moves from the response
                 moves = event['state']['moves']
             else:
-                # getting the moves from the response
                 moves = event['moves']
 
             moves = moves.split(" ")
@@ -72,15 +58,12 @@ class GameController:
             if self.color is PlayerColor.Black:
                 my_move = not my_move
 
-            # checking if it's my_move
             if my_move:
                 # checking if the last move is from my opponent
                 if len(moves) >= 1 and moves[-1] != '':
-                    # sending the opponents_move event with the move as an argument
                     move = moves[-1]
                     self.emitter.emit('opponents_move', move)
 
-                # sending the my_move event with the gameId, the moves and the move function as arguments
                 move_callback = partial(self.client.move, game_id)
                 self.emitter.emit('my_move', self.color.value, moves, move_callback)
 
