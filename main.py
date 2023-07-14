364055886ac19a6ef14127db2467e62ef487535c
Alexander <a.gerstberger@web.de>
2023-07-14 21:37:54 +0200

    removing comments that describe what the code does

--- main.py
+++ main.py
@@ -15,21 +15,16 @@ def main():
         logging.error("Missing Lichess token. Please set the environment variable LICHESS_TOKEN to your lichess token.")
         sys.exit(1)
 
-    # initializing our GameController
     game = GameController(lichess_token)
 
-    # just accepting challenges and streaming games without further processing
     game.on('challenge', game.accept_challenge)
     game.on('game_start', game.stream_game)
 
-    # initializing our Engine
     engine = Engine()
 
-    # adding the methods of the engine to handle the engines turn and the incoming moves of the opponent
     game.on('my_move', engine.move)
     game.on('opponents_move', engine.opponents_move)
 
-    # starting to watch for events
     game.watch()
 
 
