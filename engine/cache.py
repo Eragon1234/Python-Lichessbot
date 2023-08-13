133ff24c97bf2b65961898bef3ca802b32f78cfe
Alexander Gerstberger <a.gerstberger@web.de>
2023-08-13 16:05:44 +0200

    add docstrings to the Cache Protocol

--- engine/cache.py
+++ engine/cache.py
@@ -10,18 +10,28 @@ MoveEvaluation = tuple[Move, float]
 class Cache(Protocol):
     def get(self, color: Color, board: ChessBoard, min_depth: int,
             default: Optional[MoveEvaluation] = None) -> Optional[MoveEvaluation]:
-        ...
+        """
+        Gets the cached value for the given color and board if there is an
+        evaluation with a depth that's greater than or equal to min_depth.
+
+        If there isn't one, it returns default.
+        """
 
     def put(self, color: Color, board: ChessBoard,
             depth: int, value: MoveEvaluation):
-        ...
+        """
+        Puts the given value in the cache for the given color and board
+        with the given depth.
+        """
 
 
 class NoCache(Cache):
     def get(self, color: Color, board: ChessBoard, min_depth: int,
             default: Optional[MoveEvaluation] = None) -> Optional[MoveEvaluation]:
+        """Returns the default value."""
         return default
 
     def put(self, color: Color, board: ChessBoard,
             depth: int, value: MoveEvaluation):
+        """Does nothing."""
         pass
