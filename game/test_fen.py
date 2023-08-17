8502b24cd76ef27de8ec5a15e0afc82752b7f798
Alexander Gerstberger <a.gerstberger@web.de>
2023-08-17 12:44:03 +0200

    adding more fen tests

--- game/test_fen.py
+++ game/test_fen.py
@@ -6,9 +6,12 @@ from game import ChessBoard
 class TestFen(unittest.TestCase):
     def test_fen(self):
         test_fens: list[str] = [
-            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+            "6k1/3R4/8/5p2/8/1R6/3P1KP1/6N1 b - - 0 25",
+            "2r3k1/ppN2ppp/2n1bp2/8/8/8/PPPP1KPP/R1B2BNR w - - 3 12"
         ]
 
         for fen in test_fens:
-            board = ChessBoard(fen)
-            self.assertEqual(fen, board.fen())
+            with self.subTest(fen=fen):
+                board = ChessBoard(fen)
+                self.assertEqual(fen, board.fen())
