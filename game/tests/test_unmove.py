b0ad2bd9c2eee253e349c3c1f50d85c71febbfff
Alexander Gerstberger <a.gerstberger@web.de>
2023-10-16 21:42:09 +0200

    implementing hypothesis stateful testing for unmove testing

--- game/tests/test_unmove.py
+++ game/tests/test_unmove.py
@@ -1,28 +1,39 @@
 import unittest
 
+import hypothesis.strategies as st
+from hypothesis.stateful import RuleBasedStateMachine, rule, precondition
+
 from game import ChessBoard
-from game.piece.color import Color
 
 
-class TestUnmove(unittest.TestCase):
-    moveDepth = 3
+class BoardModel(RuleBasedStateMachine):
+    def __init__(self):
+        super().__init__()
+        self.board = ChessBoard()
 
-    def test_unmove(self):
-        test_fens = [
-            'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-        ]
-        for fen in test_fens:
-            with self.subTest(fen=fen):
-                board = ChessBoard(fen)
-                self.move_and_unmove(board, Color.WHITE, self.moveDepth)
+    @precondition(lambda self: len(list(self.board.legal_moves())) > 0)
+    @rule(
+        data=st.data()
+    )
+    def move(self, data: st.SearchStrategy):
+        move = data.draw(st.sampled_from(
+            sorted(self.board.legal_moves(),
+                   key=lambda m: m.uci())
+        ))
+        self.board.move(move)
 
-    def move_and_unmove(self, board: ChessBoard, color: Color, depth: int):
-        if depth == 0:
-            return
-        board_state = board.fen()
-        for move in board.legal_moves():
-            board.move(move)
-            self.move_and_unmove(board, color.enemy(), depth - 1)
-            board.unmove()
-            self.assertEqual(board.fen(), board_state,
-                             f'board state changed after unmove: {move.uci()}')
+    @precondition(lambda self: len(self.board.moves) > 0)
+    @rule()
+    def unmove(self):
+        self.board.unmove()
+
+    @rule()
+    def correct_position(self):
+        test_board = ChessBoard()
+        for move in self.board.moves:
+            test_board.move(move)
+
+        assert self.board.fen() == test_board.fen()
+
+
+TestUnmoveTestCase: unittest.TestCase = BoardModel.TestCase
